ef2be9d4408d05a104d03367310a9c1b6b3bfac3
officialvalentino <42213157+officialvalentino@users.noreply.github.com>
2018-08-11 16:21:30 +1000

    Update 1.js

--- 1.js
+++ 1.js
@@ -15,7 +15,7 @@ var vc;
 
 client.on("ready", () => {
   console.log('ready!')
-  client.user.setActivity('Prox Private Scrims', { type: 'PLAYING' });
+  client.user.setActivity('Proximity Invitational Scrims', { type: 'PLAYING' });
 })
 
 client.on('message', message => {
@@ -354,7 +354,7 @@ client.on('message', message => {
     message.guild.roles.find('name', 'Captain').members.forEach(function (user){
       if(user.voiceChannel != undefined){
         user.setVoiceChannel('474953735588544512')
-      } else {user.createDM().then(dm => dm.send('Hello! One of the staff have summoned all the captains, please check **Prox Private Scrims** when you can.')).catch(console.error);}
+      } else {user.createDM().then(dm => dm.send('Hello! One of the staff have summoned all the captains, please check **Proximity Invitational Scrims** when you can.')).catch(console.error);}
     })
   }
   //super duper special commands
